5899375258b0fb8a46fe0225a24407e6a9834a9b
brinda-s <brinda.anitha@gmail.com>
2025-01-09 16:47:44 -0500

    added exit feauture to this

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
+        
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         while(true){
             String input = scanner.nextLine().trim();
+
+            if(input.equals("exit 0")){
+                System.exit(0);
+            }
 
             if(input.isEmpty()){
                 System.out.print("$ ");
