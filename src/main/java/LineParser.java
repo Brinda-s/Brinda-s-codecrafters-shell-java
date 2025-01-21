961ed5715cd6c345b71fd523b4c303193b8cfc40
brinda-s <brinda.anitha@gmail.com>
2025-01-20 20:31:29 -0500

    backslash feauture

--- src/main/java/LineParser.java
+++ src/main/java/LineParser.java
@@ -4,67 +4,54 @@ import java.util.List;
 public class LineParser {
     public static final char SINGLE = '\'';
     public static final char DOUBLE = '"';
-
+    public static final char ESCAPE = '\\';
+    
     private final String input;
     private int index;
-
+    
     public LineParser(String input) {
         this.input = input;
         this.index = 0;
     }
-
+    
     public List<String> parse() {
         List<String> result = new ArrayList<>();
         StringBuilder currentToken = new StringBuilder();
         boolean inSingleQuotes = false;
         boolean inDoubleQuotes = false;
-        boolean lastWasQuoted = false;
+        boolean escaped = false;
         
         while (index < input.length()) {
             char c = input.charAt(index);
             
-            if (c == SINGLE && !inDoubleQuotes) {
-                inSingleQuotes = !inSingleQuotes;
-                if (!inSingleQuotes && lastWasQuoted && index + 1 < input.length() && 
-                    (input.charAt(index + 1) == SINGLE || input.charAt(index + 1) == DOUBLE)) {
-                    // Don't add to result if next char is a quote
-                    lastWasQuoted = true;
-                } else if (!inSingleQuotes) {
-                    if (currentToken.length() > 0) {
-                        result.add(currentToken.toString());
-                        currentToken.setLength(0);
-                    }
-                    lastWasQuoted = false;
+            if (escaped) {
+                // If we're in escape mode, append the current character literally
+                currentToken.append(c);
+                escaped = false;
+            } else if (c == ESCAPE) {
+                // Enter escape mode, but only if not in single quotes
+                if (!inSingleQuotes) {
+                    escaped = true;
+                } else {
+                    currentToken.append(c);
                 }
+            } else if (c == SINGLE && !inDoubleQuotes) {
+                inSingleQuotes = !inSingleQuotes;
             } else if (c == DOUBLE && !inSingleQuotes) {
                 inDoubleQuotes = !inDoubleQuotes;
-                if (!inDoubleQuotes && lastWasQuoted && index + 1 < input.length() && 
-                    (input.charAt(index + 1) == SINGLE || input.charAt(index + 1) == DOUBLE)) {
-                    // Don't add to result if next char is a quote
-                    lastWasQuoted = true;
-                } else if (!inDoubleQuotes) {
-                    if (currentToken.length() > 0) {
-                        result.add(currentToken.toString());
-                        currentToken.setLength(0);
-                    }
-                    lastWasQuoted = false;
-                }
             } else if (Character.isWhitespace(c) && !inSingleQuotes && !inDoubleQuotes) {
                 if (currentToken.length() > 0) {
                     result.add(currentToken.toString());
                     currentToken.setLength(0);
                 }
-                lastWasQuoted = false;
             } else {
                 currentToken.append(c);
-                if (inSingleQuotes || inDoubleQuotes) {
-                    lastWasQuoted = true;
-                }
             }
             
             index++;
         }
         
+        // Handle any remaining token
         if (currentToken.length() > 0) {
             result.add(currentToken.toString());
         }
